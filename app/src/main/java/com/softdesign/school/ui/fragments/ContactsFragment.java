8a53a246fed6449bca265a1cc5a651986b9009e3
ViBelov <vikking911@yandex.ru>
2016-03-13 18:54:51 +0400

    - диалоговые окна добавления пользователей и команд - работа с БД - ListView loader ...

--- app/src/main/java/com/softdesign/school/ui/fragments/ContactsFragment.java
+++ app/src/main/java/com/softdesign/school/ui/fragments/ContactsFragment.java
@@ -1,34 +1,55 @@
 package com.softdesign.school.ui.fragments;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.design.widget.NavigationView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
+import com.activeandroid.query.Select;
 import com.softdesign.school.R;
+import com.softdesign.school.data.storage.model.Team;
+import com.softdesign.school.data.storage.model.User;
 import com.softdesign.school.ui.activities.MainActivity;
 import com.softdesign.school.ui.adapters.ContactAdapter;
-import com.softdesign.school.data.storage.model.User;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
-public class ContactsFragment extends Fragment {
-    ArrayList<User> mUsers = new ArrayList<User>();
+public class ContactsFragment extends Fragment implements LoaderManager.LoaderCallbacks<List<User>>   {
+    //ArrayList<User> mUsers = new ArrayList<User>();
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+    private EditText mFirstName;
+    private EditText mLastName;
+    private EditText mTeam;
+    private Spinner mSpinner;
     RecyclerView listContacts;
     View mainView;
+    View dialogView;
+    View dialogView1;
+    List<User> mUsers;
+    List<Team> mTeams;
+
+
     public ContactsFragment() {
         this.setRetainInstance(true); // непересоздавать фрагмент при повороте экрана
     }
@@ -36,8 +57,10 @@ public class ContactsFragment extends Fragment {
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        generateData();
+        loadData();
         mAdapter = new ContactAdapter(mUsers);
+
+
     }
     @Nullable
     @Override
@@ -51,8 +74,19 @@ public class ContactsFragment extends Fragment {
         navigationView.getMenu().findItem(R.id.drawer_contacts).setChecked(true);
         getActivity().setTitle(getResources().getString(R.string.fragment_contacts_title));
 
-        generateData();
-        //добавление контактов
+
+        dialogView = inflater.inflate(R.layout.dialog_add_user,null,false);
+        dialogView1 = inflater.inflate(R.layout.dialog_add_team,null,false);
+
+        mFirstName = (EditText) dialogView.findViewById(R.id.first_name_add);
+        mLastName = (EditText) dialogView.findViewById(R.id.last_name_add);
+        mTeam = (EditText) dialogView1.findViewById(R.id.team_add_name);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, getAllNames());
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        final Spinner spinner = (Spinner) dialogView.findViewById(R.id.team_spinner_add);
+        spinner.setAdapter(adapter);
+
+
         listContacts = (RecyclerView) mainView.findViewById(R.id.list_view);
         listContacts.setHasFixedSize(true);
 
@@ -63,7 +97,26 @@ public class ContactsFragment extends Fragment {
 
         ((MainActivity) getActivity()).lockAppBar(true);
         return mainView;
+
+
+
     }
+
+    private void saveData(){
+        User user = new User(mFirstName.getText().toString(),mLastName.getText().toString(),null);
+        user.save();
+        mFirstName.getText();
+
+    }
+
+    private void saveData1(){
+        Team team = new Team(mTeam.getText().toString());
+        team.save();
+        mTeam.getText();
+
+    }
+
+
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
@@ -75,14 +128,125 @@ public class ContactsFragment extends Fragment {
         fab.setLayoutParams(params);
         fab.setImageResource(R.drawable.ic_add_24dp);
         fab.show();
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setTitle("Создать команду или пользователя?")
+                        .setCancelable(false)
+                        .setPositiveButton("Создать пользователя",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                                        builder.setTitle("Создать пользователя")
+                                                .setCancelable(false)
+                                                .setPositiveButton("Готово",
+                                                        new DialogInterface.OnClickListener() {
+                                                            public void onClick(DialogInterface dialog, int which) {
+                                                                saveData();
+                                                                getLoaderManager().initLoader(0, null, ContactsFragment.this).forceLoad();
+                                                                dialog.cancel();
+
+                                                            }
+
+                                                        })
+
+                                                .setNegativeButton("Отмена", new DialogInterface.OnClickListener() {
+                                                    public void onClick(DialogInterface dialog, int which) {
+                                                        dialog.cancel();
+                                                    }
+                                                })
+                                                .setView(dialogView);
+                                        AlertDialog alertDialog = builder.create();
+                                        alertDialog.show();
+
+                                    }
+
+                                })
+
+                        .setNegativeButton("Создать команду", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                                builder.setTitle("Создать команду")
+                                        .setCancelable(false)
+                                        .setPositiveButton("Готово",
+                                                new DialogInterface.OnClickListener() {
+                                                    public void onClick(DialogInterface dialog, int which) {
+                                                        //mTeams = getDataListTeams();
+                                                        saveData1();
+                                                        getLoaderManager().initLoader(0, null, ContactsFragment.this).forceLoad();
+                                                        dialog.cancel();
+
+                                                    }
+
+                                                })
+
+                                        .setNegativeButton("Отмена", new DialogInterface.OnClickListener() {
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                dialog.cancel();
+                                            }
+                                        })
+                                        .setView(dialogView1);
+                                AlertDialog alertDialog = builder.create();
+                                alertDialog.show();
+                            }
+                        });
+
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+
+
+            }
+        });
+
+    }//
+
+    public List<String> getAllNames() {
+        List<Team> teams = getDataListTeams();
+        List<String> names = new ArrayList<>(teams.size());
+        for (Team t : teams) {
+            names.add(t.mTeam);
+        }
+        return names;
+    }
+
+    public void loadData() {
+        mUsers = getDataListUsers();
+        mTeams = getDataListTeams();
 
     }
-    //генерируем контакты
-    private void generateData() {
-        mUsers.add(new User(getResources().getDrawable(R.drawable.ic_account_box_24dp), "Иванов", "Иван"));
-        mUsers.add(new User(getResources().getDrawable(R.drawable.ic_account_box_24dp), "Петров", "Петр"));
-        mUsers.add(new User(getResources().getDrawable(R.drawable.ic_account_box_24dp), "Николаев", "Николай"));
-        mUsers.add(new User(getResources().getDrawable(R.drawable.ic_account_box_24dp), "Стас", "Михайлов"));
+
+    public List<User> getDataListUsers() {
+        return new Select()
+                .from(User.class)
+                .execute();
+    }
+
+    public List<Team> getDataListTeams() {
+        return new Select()
+                .from(Team.class)
+                .execute();
+    }
+
+    @Override
+    public Loader<List<User>> onCreateLoader(int id, Bundle args) {
+        return new AsyncTaskLoader<List<User>>(getContext()) {
+            @Override
+            public List<User> loadInBackground() {
+            return getDataListUsers();
             }
+        };
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<User>> loader, List<User> data) {
+        mAdapter = new ContactAdapter(data);
+        listContacts.setAdapter(mAdapter);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<User>> loader) {
+
+    }
 }
 
